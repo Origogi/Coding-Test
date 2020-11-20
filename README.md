036c3e2006f8a76f82849aaeab9d272ea9cbb778
origogi <rlawjdxo1212@naver.com>
2020-11-20 10:09:04 +0900

    add ransom note

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 코딩 테스트 연습 문제를 정리한 레포지토리입니다.
 
-- Codilty
-- Programmers
-- Backjoon Online Judge
+## Codilty
+
+## Programmers
+
+## Backjoon Online Judge(BOJ)
+
+## HackerRank
+
+- Hash Tables: Ransom Note
+    - [Link](https://www.hackerrank.com/challenges/ctci-ransom-note/problem)
+    - [Code](src/hackerrank/RansomNote.java)
+  
+
--- src/hackerrank/practice/RansomNote.java
+++ src/hackerrank/practice/RansomNote.java
@@ -1,4 +1,36 @@
 package hackerrank.practice;
 
+import java.util.HashMap;
+
+
+// https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 public class RansomNote {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String keyword : magazine) {
+            map.put(keyword, map.getOrDefault(keyword,0) + 1);
+        }
+
+        for (String keyword : note) {
+            int count = map.getOrDefault(keyword, 0);
+
+            if (count == 0) {
+                System.out.println("No");
+                return;
+            }
+            count--;
+            map.put(keyword,count);
+        }
+
+        System.out.println("Yes");
+    }
 }
