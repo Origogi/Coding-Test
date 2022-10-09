6ca06c0cb0d5147e24f82766a32c8142a5ae5763
Origogi <rlawjdxo1212@naver.com>
2022-10-09 20:29:56 +0900

    Add plusOne

---
+++ src/leetcode/javascript/plusOne.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/plus-one/submissions/
+
+var plusOne = function(digits) {
+
+    let result = [...digits];
+
+    result[result.length-1] = result[result.length -1] +1;
+
+    var i = result.length -1;
+
+    while(i > 0 && result[i] >= 10) {
+      result[i] -= 10;
+      result[i - 1] += 1;
+      i--;
+    }
+
+    if (result[0] >= 10) {
+        result[i] -= 10;
+        result = [1,...result];
+    }
+
+    return result;
+};
